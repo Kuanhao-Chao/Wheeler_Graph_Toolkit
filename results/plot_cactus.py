532a16a7f9a63cbab284959ea9b07d8e14bc2fd4
perry0513 <perrypeiweichen@gmail.com>
2022-08-26 10:11:22 -0700

    Plot cactus plots

---
+++ results/plot_cactus.py
@@ -0,0 +1,32 @@
+import sys
+from matplotlib import pyplot as plt
+import itertools
+
+marker = itertools.cycle((',', '+', '.', 'o', '*')) 
+n_instances = 0
+
+all_times = []
+
+for file in sys.argv[1:]:
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        times = [float(line.split()[2]) for line in lines]
+
+    sorted_times = sorted(times)
+
+    agg_times = [ sorted_times[0] ]
+    for i in range(1, len(sorted_times)):
+        agg_times.append(agg_times[i-1] + sorted_times[i])
+
+    n_instances = max(n_instances, len(agg_times))
+
+    all_times.append((agg_times, file))
+
+
+
+plt.xlabel('# of instances')
+plt.ylabel('time (msec)')
+for agg_times, label in all_times:
+    plt.plot(range(len(agg_times)), agg_times, label=label, marker=next(marker))
+plt.legend()
+plt.show()
