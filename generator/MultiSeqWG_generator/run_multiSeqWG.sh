dc2c064ea24402cba03649f68e341c47b76ef583
Kuan-Hao Chao <kh.chao@salz3.idies.jhu.edu>
2022-10-18 19:41:19 -0400

    Remove bin/ and some testing generators

--- generator/MultiSeqWG_generator/run_multiSeqWG.sh
+++
@@ -1,7 +0,0 @@
-python firstseq_2_dot_multiSeqWG.py ../../multiseq_alignment/a$1.fasta
-
-../../bin/recognizer ../../graph/multiSeqWG/multiSeqWG_a$1.dot -w --outDir ../../graph/multiSeqWG/
-
-python create_multiSeqWG.py ../../multiseq_alignment/a$1.fasta ../../graph/multiSeqWG/__multiSeqWG_a$1/nodes.txt
-
-../../bin/recognizer ../../graph/multiSeqWG/a$1.dot -b
--- generator/PrefixSortedGraph/Run_Fasta_2_Dot.sh
+++
@@ -1,5 +0,0 @@
-#search_dir=../../multiseq_alignment/
-for entry in "$1/"*
-do
-    	python create_RevDetAuto.py $entry
-done
